3d06e80a50379c584f759ac674b5165b9afbd549
ryan woodsmall <rwoodsmall@gmail.com>
2015-02-19 10:46:20 -0600

    dropbear current -> version symlink

--- scripts/dropbear_install.sh
+++ scripts/dropbear_install.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+echo ${UID} | grep -q ^0$
+if [ $? -ne 0 ] ; then
+    echo "you need to be root" 1>&2
+    exit 1
+fi
+
 make \
   MULTI=1 \
   SCPPROGRESS=1 \
   PROGRAMS="dropbear dbclient dropbearkey dropbearconvert scp" \
   install
+
+test -e /opt/dropbear/etc || mkdir /opt/dropbear/etc
+test -e /opt/dropbear/previous && rm -f /opt/dropbear/previous
+test -e /opt/dropbear/current && mv /opt/dropbear/current /opt/dropbear/previous
+ln -s /opt/dropbear/`basename ${PWD}` /opt/dropbear/current
