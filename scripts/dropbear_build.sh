73c4802acb7c6d6f6a82627087fc5bac523e6ec9
ryan woodsmall <rwoodsmall@gmail.com>
2015-12-14 21:54:38 +0000

    use parallel make for building Dropbear; copies of options.h for Dropbear 2015.69, 2015.70

--- scripts/dropbear_build.sh
+++ scripts/dropbear_build.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+# how many parallel jobs
+if `which nproc >/dev/null 2>&1` ; then
+  MJOBS=`nproc`
+elif `which lscpu >/dev/null 2>&1` ; then
+  MJOBS=`lscpu | awk '/^CPU\(s\)/{print $NF}'`
+else
+# check /proc/cpuinfo (arm format is different)
+# check getconf (glibc only)
+  MJOBS=1
+fi
+
 make \
+  -j${MJOBS} \
   MULTI=1 \
   SCPPROGRESS=1 \
   STATIC=1 \
