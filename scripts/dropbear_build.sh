3f8c8549b2630706152d00e76530fcb62e774c27
ryan woodsmall <rwoodsmall@gmail.com>
2015-12-16 21:45:56 +0000

    use /proc/cpuinfo if no nproc/lscpu; tested on CentOS x86_64 and ARM Poky

--- scripts/dropbear_build.sh
+++ scripts/dropbear_build.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+# default to only one parallel make job
+MJOBS=1
+# XXX - check getconf (glibc only)
 # how many parallel jobs
 if `which nproc >/dev/null 2>&1` ; then
   MJOBS=`nproc`
 elif `which lscpu >/dev/null 2>&1` ; then
   MJOBS=`lscpu | awk '/^CPU\(s\)/{print $NF}'`
-else
-# check /proc/cpuinfo (arm format is different)
-# check getconf (glibc only)
-  MJOBS=1
+elif [ `xargs < /proc/cpuinfo | grep -o 'processor : ' | wc -l` -gt 1 ] ; then
+  MJOBS=`xargs < /proc/cpuinfo | grep -o 'processor : ' | wc -l`
 fi
 
 make \
